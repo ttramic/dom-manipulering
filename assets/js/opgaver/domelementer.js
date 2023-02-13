77f61662ae5280e16f17610767e3646b5aa950b6
ttramic <vipmanden@gmail.com>
2023-02-13 11:26:46 +0100

    nynyny

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -8,6 +8,10 @@ Du skal finde elementet med id="yellowFigure" og console logge det.
 */
 
 // din kode her
+const redFigure = document.getElementById("redFigure");
+const yellowFigure = document.getElementById("yellowFigure");
+
+console.log(redFigure, yellowFigure);
 
 
 /* opgave 1.2
@@ -17,7 +21,8 @@ farven på elementerne skal være blå. brug evt. elelement.style.backgroundColo
 
 
 // din kode her
-
+redFigure.style.backgroundColor = "blue";
+yellowFigure.style.backgroundColor = "blue";
 
 
 /* opgave 2.1
@@ -28,6 +33,18 @@ Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
 
 
 // din kode her
+const opgaveTwo = document.getElementById("opgaveTwo");
+let myHeader = document.createElement("h2");
+myHeader.innerText = "Opgave 2.1 løsning"
+
+let myText = document.createElement("p");
+myText.innerText = "Jeg har løst opgave 2.1"
+
+opgaveTwo.appendChild(myHeader);
+opgaveTwo.appendChild(myText)
+
+
+
 
 
 // opgave 3 liveHTML lists
@@ -36,7 +53,8 @@ Du skal finde alle elementer med klassen purpleFigures og console logge resultat
 */
 
 // din kode her
-
+const myPurpleFigures = document.getElementsByClassName('purpleFigures');
+console.log(myPurpleFigures)
 
 /* opgave 3.2
 Du skal finde alle elementer med klassen purpleFigures og bruge array.from() til at konvertere listen
@@ -46,6 +64,10 @@ array.from beskrivelse HER: https://developer.mozilla.org/en-US/docs/Web/JavaScr
 
 // din kode her
 
+let myArray = Array.from(myPurpleFigures);
+myArray.map((myElement) => {
+    myElement.style.backgroundColor = "red";
+});
 
 /* opgave 3.3
 Brug myLiveList til at ændre h3 elementets, inden i purpleFigures, tekst til RED. du kan bruge
@@ -54,7 +76,9 @@ myElement.children[0].innerHTML til at ændre 1. child elements inner html.
 
 
 // din kode her
-
+myArray.map((myElement) => {
+    myElement.children[0].innerHTML = "red"
+});
 
 
 /* opgave 4
@@ -71,6 +95,25 @@ const myData = {
 
 // din kode her
 
+const myFigure = document.getElementById('opgaveFour');
 
+
+let petName = document.createElement('h2');
+    petName.innerHTML = myData.name;
+
+let petImg = document.createElement('img');
+    petImg.setAttribute("src", petImg)
+    petImg.src = myData.image;
+    
+    
+        
+let petDisc = document.createElement('p');
+    petDisc.innerHTML = myData.description;
+
+
+
+myFigure.appendChild(petName)
+myFigure.appendChild(petImg)
+myFigure.appendChild(petDisc)
 
 
